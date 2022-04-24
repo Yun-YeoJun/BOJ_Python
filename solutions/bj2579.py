52a8558787e33db45b309e6e2cfd83ddef4bb5f5
Yun-YeoJun <yyj8771@naver.com>
2022-04-25 00:48:00 +0900

    modify bj2579.py

--- solutions/bj2579.py
+++ solutions/bj2579.py
@@ -1,35 +1,38 @@
 from sys import stdin
 
-n = int(stdin.readline()) # number of stairs
+n = int(stdin.readline())
 
-l = [0] * n # score of each stair
+score = [0] * n
 
 for i in range(n):
-    l[i] = int(stdin.readline())
+    score[i] = int(stdin.readline())
 
-result = [-1] * (n) # reset to -1
-cnt = [0] * (n)
-cur = 0
+result = [[-1 for i in range(2)] for j in range(n+1)]
 
-l = list(reversed(l))
+if n >= 3:
+    for i in range(2):
+        result[n][i] = score[n - 1]
+        result[n - 1][i] = score[n - 2] + result[n][0]
+        result[n - 2][i] = score[n - 3] + result[n][0]
 
-result[0] = l[0] + l[1]
-result[1] = l[0] + l[2]
+    def maxScoreSum(result, k, tf):
+        if result[k] == [-1,-1]:
+            if k > 0:
+                result[k][0] = max(maxScoreSum(result, k + 1, 1),maxScoreSum(result, k + 2, 0)) + score[k - 1]
+                result[k][1] = maxScoreSum(result, k + 2, 0) + score[k - 1]
+                return result[k][tf]
+            else:
+                result[k] = max(maxScoreSum(result, k + 1, 0),maxScoreSum(result, k + 2, 0))
+                return result[k]
+        else:
+            if tf == 0:
+                return max(result[k][0], result[k][1])
+            else:
+                return result[k][1]
 
-cnt[0] += 1
+    maxScoreSum(result, 0, 0)
 
-for i in range(2, n-1):
-    tcnt = cnt[i - 1]
-    r1 = result[i - 1]
-    r2 = result[i - 2]
-    if tcnt == 1:
-        result[i] = r2 + l[i + 1]
-    elif r1 > r2:
-        result[i] = r1 + l[i + 1]
-        cnt[i] = cnt[i - 1] + 1
-    elif r1 < r2:
-        result[i] = r2 + l[i + 1]
-    else:
-        result[i] = r2 + l[i + 1]
+    print(result[0])
 
-print(result[n-1])
+else:
+    print(sum(score))
