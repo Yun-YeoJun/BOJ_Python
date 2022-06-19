e3ed33acf78469895b26575fa4a5bff485be3452
Yun-YeoJun <yyj8771@naver.com>
2022-06-19 16:57:43 +0900

    modify bj11727.py

--- solutions/bj11727.py
+++ solutions/bj11727.py
@@ -1,11 +1,11 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
 n = int(input())
 dp = [0 for i in range(n)]
 dp[0] = 1
-dp[1] = 3
+if n > 1:
+    dp[1] = 3
 
 
 for i in range(2,n):
